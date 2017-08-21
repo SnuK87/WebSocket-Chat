87178ce4db2d03b85ac31e5f8899c966f2bc31c6
SnuK <d.griese1987@gmail.com>
2017-08-22 00:24:22 +0900

    reorganize

---
+++ chat-app/src/main/java/de/snuk/websocket/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package de.snuk.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    // @Autowired
+    // private WebSocketMessageBrokerStats webSocketMessageBrokerStats;
+    //
+    // @PostConstruct
+    // public void init() {
+    // webSocketMessageBrokerStats.setLoggingPeriod(10 * 1000);
+    // }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+	config.enableSimpleBroker("/topic");
+	config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+	registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+}
