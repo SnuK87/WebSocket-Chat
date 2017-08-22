3e6db8b60ef4b6a655dd61cb9596c08ea9fd6481
SnuK <d.griese1987@gmail.com>
2017-08-23 00:10:53 +0900

    extend message handler

--- without-stomp/src/main/java/de/snuk/websocketdemo/MyBinaryHandler.java
+++ without-stomp/src/main/java/de/snuk/websocketdemo/MyBinaryHandler.java
@@ -1,7 +1,8 @@
 package de.snuk.websocketdemo;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.BinaryMessage;
@@ -11,12 +12,19 @@ import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 @Component
 public class MyBinaryHandler extends BinaryWebSocketHandler {
 
-	@Override
-	public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws IOException {
-		System.out.println("incoming message: " + message.getPayload());
-		ByteBuffer payload = message.getPayload();
-		byte[] array = payload.array();
-		System.out.println(new String(array));
-		session.sendMessage(new BinaryMessage("hello wordl".getBytes()));
+    private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+	sessions.add(session);
+    }
+
+    @Override
+    public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws IOException {
+	System.out.println("Incoming message: " + new String(message.getPayload().array()));
+
+	for (WebSocketSession s : sessions) {
+	    s.sendMessage(new BinaryMessage("just a simple binary response".getBytes()));
 	}
+    }
 }
--- without-stomp/src/main/java/de/snuk/websocketdemo/MyTextHandler.java
+++ without-stomp/src/main/java/de/snuk/websocketdemo/MyTextHandler.java
@@ -1,6 +1,8 @@
 package de.snuk.websocketdemo;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -10,10 +12,19 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class MyTextHandler extends TextWebSocketHandler {
 
-	@Override
-	public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-		System.out.println("incoming message: " + message.getPayload());
-		TextMessage msg = new TextMessage("Response: " + message.getPayload());
-		session.sendMessage(msg);
+    private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+	sessions.add(session);
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+	System.out.println("Incoming message: " + message.getPayload());
+
+	for (WebSocketSession s : sessions) {
+	    s.sendMessage(new TextMessage("just a simple text response"));
 	}
+    }
 }
