46421158018049afd908f818e34f2ff5c465dfb3
SnuK <d.griese1987@gmail.com>
2017-08-23 00:12:39 +0900

    init secure project

---
+++ secure-websocket/src/main/java/de/snuk/securesocket/SecureWebsocketApplication.java
@@ -0,0 +1,12 @@
+package de.snuk.securesocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWebsocketApplication.class, args);
+	}
+}
