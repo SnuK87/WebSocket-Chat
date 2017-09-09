311e683cbe729d273ea2a7537e502470faf6e58b
SnuK <d.griese1987@gmail.com>
2017-09-10 01:44:34 +0900

    update

---
+++ websocket-custom-broker/src/main/java/de/snuk/wsbroker/WebsocketCustomBrokerApplication.java
@@ -0,0 +1,12 @@
+package de.snuk.wsbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketCustomBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketCustomBrokerApplication.class, args);
+	}
+}
---
+++ websocket-custom-broker/src/test/java/de/snuk/wsbroker/WebsocketCustomBrokerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.snuk.wsbroker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsocketCustomBrokerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
